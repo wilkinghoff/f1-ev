03bfe978bdef1ac13ea35ef5ca79f84b31d7800e
Kevin Wilkinghoff <kwilk90@gmail.com>
2023-06-13 18:17:35 +0200

    update script to compute all performance measures

--- f1_ev.py
+++ f1_ev.py
@@ -1,6 +1,11 @@
 import numpy as np
 from sklearn import metrics
 import matplotlib.pyplot as plt
+import argparse
+import pandas as pd
+from tqdm import tqdm
+import os
+from scipy.stats import hmean
 
 
 def f1_ev(y_true, pred, bounded=False):
@@ -19,63 +24,68 @@ def f1_ev(y_true, pred, bounded=False):
     f1_scores = np.zeros(thresholds.shape)
     for k, threshold in enumerate(thresholds):
         f1_scores[k] = metrics.f1_score(y_true, pred > threshold)
-    print(thresholds)
-    print(f1_scores)
     # integrate using Riemann sum to get expected value of F1 score
     ds = np.diff(thresholds/ (thresholds[-1] - thresholds[0]))
-    #df1 = (f1_scores[1:]+f1_scores[:-1])/2.
     df1 = f1_scores[:-1]
     return np.sum(ds*df1), np.max(f1_scores)
 
-pred = np.array([1,3,5,7,11,13,17,19])
-y = np.array([0, 0, 1, 0, 0, 1, 1, 1])
-#pred = np.array([0., 0.5, 1., 1.1, 1.2])
-#y = np.array([0, 0, 1, 1, 1])
-#pred = np.random.rand(100)
-#y = np.round(pred+np.random.rand(100)*0.1-np.random.rand(100)*0.1)
-#pred[y==1] +=0.5
-#pred[y==0] += -0.5
 
-fpr, tpr, thresholds = metrics.roc_curve(y, pred, pos_label=1, drop_intermediate=False)  # pos_label needs to be modified!
-thresholds[0] = thresholds[1]+1e-16  # correct manually included extra threshold to be only slightly higher, impact should be very minor (or even non-existant?) for final score
-#fpr, tpr, thresholds = metrics.precision_recall_curve(y, pred, pos_label=1)  # pos_label needs to be modified!
-#end_idx = np.argmax(tpr>=1)
-#tpr = tpr[:end_idx+1]
-#fpr = fpr[:end_idx+1]
-#thresholds = thresholds[:end_idx+1]
+def compute_performance(pred_files_path, ref_files_path):
+    # load predictions
+    pred_files = os.listdir(pred_files_path)
+    pred_files_cleaned = []
+    dec_files = []
+    # remove decision results
+    for k in np.arange(len(pred_files)):
+        if pred_files[k].startswith('anomaly_score'):
+            pred_files_cleaned.append(pred_files[k])
+        else:
+            dec_files.append(pred_files[k])
+    pred_files = pred_files_cleaned
+    # load ground truth
+    ref_files = os.listdir(ref_files_path)
+    auc = np.zeros(len(pred_files))
+    pauc = np.zeros(len(pred_files))
+    f1ev = np.zeros(len(pred_files))
+    maxf1 = np.zeros(len(pred_files))
+    f1ev_bounded = np.zeros(len(pred_files))
+    estf1 = np.zeros(len(pred_files))
+    subf1 = np.zeros(len(pred_files))
+    for k in tqdm(np.arange(len(pred_files))):
+        pred = np.array(pd.read_csv(pred_files_path+pred_files[k]))[:,1].astype(np.float32)
+        dec = np.array(pd.read_csv(pred_files_path+dec_files[k]))[:,1].astype(np.float32)
+        y_true = np.array(pd.read_csv(ref_files_path+ref_files[k]))[:,1].astype(np.float32)
+        # compute AUC
+        auc[k] = metrics.roc_auc_score(y_true, pred)
+        # compute pAUC
+        pauc[k] = metrics.roc_auc_score(y_true, pred, max_fpr=0.1)
+        # computet F1-EV
+        f1ev[k], maxf1[k] = f1_ev(y_true, pred)
+        # compute bounded F1-EV
+        f1ev_bounded[k], _ = f1_ev(y_true, pred, bounded=True)
+        # estimate threshold and compute F1-score
+        normal_scores = pred[y_true==0]
+        alpha = 1.28
+        threshold = np.mean(normal_scores) + alpha*np.std(normal_scores)
+        estf1[k] = metrics.f1_score(y_true, pred>threshold)
+        # submitted F1-score
+        subf1[k] = metrics.f1_score(y_true, dec)
+    print('threshold-independent metrics:')
+    print('harmonic mean of AUCs: ' + str(hmean(auc)))
+    print('harmonic mean of pAUCs: ' + str(hmean(pauc)))
+    print('harmonic mean of F1-EVs: ' + str(hmean(f1ev)))
+    print('harmonic mean of bounded F1-EVs: ' + str(hmean(f1ev_bounded)))
+    print('threshold-dependent metrics:')
+    print('submitted F1-score: ' + str(hmean(subf1)))
+    print('estimated F1-score: ' + str(hmean(estf1)))
+    print('optimal F1-score: ' + str(hmean(maxf1)))
+    return
 
-print(thresholds)
-#print(tpr)
-#print(fpr)
 
-f1_scores = np.zeros(thresholds.shape)
-for k, threshold in enumerate(thresholds):
-    f1_scores[k] = metrics.f1_score(y, pred>=threshold)
-print(f1_scores)
-
-thresholds = thresholds/(thresholds[0]-thresholds[-1])
-dx=np.diff(fpr)
-ds=np.diff(-thresholds)
-dy=(tpr[1:]+tpr[:-1])/2
-df1 = (f1_scores[1:]+f1_scores[:-1])/2
-
-#print(dx)
-#print(dy)
-print(ds)
-print(df1)
-#print(dy*dx*ds)
-
-
-f1_ev_score, f1_max = f1_ev(y_true=y, pred=pred)
-print('F1-EV is ' + str(f1_ev_score))
-print('Optimal F1-score is ' + str(f1_max))
-print('AUC is ' + str(np.sum(dy*dx)))
-print('TA-AUC is ' + str(np.sum(dy*dx*ds)))  # replace with square-root of dx*dy?
-#print('TA-AUC is ' + str(np.sum(dy*ds)))  # replace with square-root of dx*dy?
-#print('TA-AUC is ' + str(np.sum(dy*np.sqrt(dx*ds))))  # replace with square-root of dx*dy?
-#display = metrics.RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=np.sum(dy*dx), estimator_name='dx sy')
-#display = metrics.RocCurveDisplay(fpr=thresholds, tpr=tpr, roc_auc=np.sum(dy*ds), estimator_name='dy ds')
-#display = metrics.RocCurveDisplay(fpr=fpr, tpr=thresholds, roc_auc=np.sum(dx*ds), estimator_name='dx ds')
-#display = metrics.RocCurveDisplay(fpr=thresholds, tpr=f1_scores, roc_auc=np.sum(df1*ds), estimator_name='dx sy')
-#display.plot()
-#plt.show()
+if __name__ == "__main__":
+    # example: python f1_ev.py -pred_files_path ./dcase-2022/team_fkie/ -ref_files_path ./dcase-2022/ground_truth_data/
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-pred_files_path', type=str, help='path to the folder containing the prediction files')
+    parser.add_argument('-ref_files_path', type=str, help='path to the folder containing the ground truth files')
+    args = parser.parse_args()
+    compute_performance(args.pred_files_path, args.ref_files_path)
