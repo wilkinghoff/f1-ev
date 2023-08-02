bb5dfa81697173750a0f771b40ebb0b63f6924b5
Kevin Wilkinghoff <kwilk90@gmail.com>
2023-08-02 21:04:04 +0200

    added using different alphas

--- f1_ev.py
+++ f1_ev.py
@@ -5,6 +5,7 @@ import pandas as pd
 from tqdm import tqdm
 import os
 from scipy.stats import hmean, gamma, pearsonr
+import matplotlib.pyplot as plt
 
 
 def store_results(x1, x2, filepath):
@@ -22,14 +23,13 @@ def estimate_decision_threshold(pred, dec):
     return thresholds[np.argmax(similarities)]
 
 
-def f1_ev(y_true, pred, bounded=False, orig_threshold=None):
+def f1_ev(y_true, pred, bounded=False, orig_threshold=None, alpha=0.2515):
     # define thresholds
     thresholds = np.unique(pred)
     if bounded:
         normal_scores = pred[y_true==0]
         std_normal = np.std(normal_scores)
         # define bounds for threshold estimated with standard deviation
-        alpha = 0.2515
         est_upper_bound = orig_threshold+alpha*std_normal
         est_lower_bound = np.mean(normal_scores)-alpha*std_normal
         thresholds = np.concatenate([np.expand_dims(est_lower_bound, axis=0), thresholds[(thresholds<est_upper_bound)*(thresholds>est_lower_bound)], np.expand_dims(est_upper_bound, axis=0)], axis=0)
@@ -41,6 +41,37 @@ def f1_ev(y_true, pred, bounded=False, orig_threshold=None):
     ds = np.diff(thresholds/ (thresholds[-1] - thresholds[0]))
     df1 = f1_scores[:-1]
     return np.sum(ds*df1), np.max(f1_scores), thresholds[np.argmax(f1_scores)]
+
+
+def test_alpha_values(pred_files_path, ref_files_path):
+    # load predictions
+    pred_files = os.listdir(pred_files_path)
+    pred_files_cleaned = []
+    dec_files = []
+    # remove decision results
+    for k in np.arange(len(pred_files)):
+        if pred_files[k].startswith('anomaly_score'):
+            pred_files_cleaned.append(pred_files[k])
+        else:
+            dec_files.append(pred_files[k])
+    pred_files = pred_files_cleaned
+    # load ground truth
+    ref_files = os.listdir(ref_files_path)
+
+    alphas = np.arange(0.025, 1.025, 0.025)
+    f1ev_bounded = np.zeros((len(pred_files), alphas.shape[0]))
+    estf1 = np.zeros(len(pred_files))
+    subf1 = np.zeros(len(pred_files))
+    for k in np.arange(len(pred_files)):
+        pred = np.array(pd.read_csv(pred_files_path+pred_files[k]))[:,1].astype(np.float32)
+        dec = np.array(pd.read_csv(pred_files_path+dec_files[k]))[:,1].astype(np.float32)
+        y_true = np.array(pd.read_csv(ref_files_path+ref_files[k]))[:,1].astype(np.float32)
+        # compute optimal threshold
+        _, _, threshold = f1_ev(y_true, pred)
+        if np.array_equal(dec, dec.astype(bool)):
+            for j, alpha in enumerate(alphas):
+                f1ev_bounded[k,j], _, _ = f1_ev(y_true, pred, bounded=True, orig_threshold=threshold, alpha=alpha)
+    return alphas, f1ev_bounded
 
 
 def compute_performance(pred_files_path, ref_files_path, verbose=False):
@@ -104,6 +135,15 @@ if __name__ == "__main__":
     parser.add_argument('-ref_files_path', type=str, help='path to the folder containing the ground truth files')
     args = parser.parse_args()
 
+    # compute bounded F1-EV for different values of alpha
+    alpha_results = []
+    for team_dir in tqdm(os.listdir(args.pred_files_path)):
+        for submission_dir in os.listdir(args.pred_files_path + '/' + team_dir + '/'):
+            alphas, alpha_result = test_alpha_values(args.pred_files_path + '/' + team_dir + '/' + submission_dir + '/', args.ref_files_path)
+            alpha_results.append(alpha_result)
+    alpha_results = np.mean(np.concatenate(alpha_results), axis=0)
+    store_results(alphas, alpha_results, 'alpha_vs_f1-ev-bounded.txt')
+
     # compute all metrics for all files
     results = []
     for team_dir in tqdm(os.listdir(args.pred_files_path)):
@@ -118,6 +158,7 @@ if __name__ == "__main__":
     f1ev_bounded = np.ravel(results[:,3])
     f1_sub = np.ravel(results[:,4])
     f1_opt = np.ravel(results[:,5])
+    """
     valid = f1_sub>0
     auc = auc[valid]
     pauc = pauc[valid]
@@ -125,7 +166,7 @@ if __name__ == "__main__":
     f1ev_bounded = f1ev_bounded[valid]
     f1_sub = f1_sub[valid]
     f1_opt = f1_opt[valid]
-
+    """
     # output results
     print('Pearson correlation coefficients:')
     print('AUC-ROC vs. F1-EV: ' + str(np.round(pearsonr(auc, f1ev)[0], 3)))
